13d8fc449ea9a60cc6cbf629d4d9de862b5454ca
Alok Mathur <73957024+alok27a@users.noreply.github.com>
2021-05-12 17:23:22 +0530

    12/5/20

---
+++ Lab/C++ Programs/inheritanceBasics.cpp
@@ -0,0 +1,23 @@
+/*
+Gaining/Obtaining few information from one class by another
+There is a base class(baap) & there is a derived class(beta).
+
+Inheritance types:-
+1)Single Inheritance---> 1 Baap -> 1 Beta
+2)Multiple Inheritance---> 2 or more Baap ->1 Beta
+3)Heiarchal Inheritance---> 1 Baap->2 Beta
+4)Multilevel Inheritance---> 1Grandfather->1 Baap ->1 Beta 
+5)Hybrid inheritance---> (Multiple+ Multilevel)Inheritance 
+*/
+
+#include <stdio.h>
+#include <iostream>
+#include <string.h>
+ 
+using namespace std;
+
+int main()
+{
+
+    return 1;
+}
